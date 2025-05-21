daaa26cf0a9ada03c0fb159f388f24aa13f7fbab
Rafa-Gamero <rafagameroarrabal@gmail.com>
2025-05-21 15:28:15 +0200

    solution lab

--- gateway/src/main/java/com/api_gateway/demo/config/GatewayConfiguration.java
+++ gateway/src/main/java/com/api_gateway/demo/config/GatewayConfiguration.java
@@ -1,32 +1,20 @@
 package com.api_gateway.demo.config;
 
-
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GatewayConfiguration {
+public class GatewayConfiguration{
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route("student_route", r -> r
-                        .path("/api/students/**")
-                        .filters(f -> f.rewritePath("/api/students/(?<segment>.*)", "/students/${segment}"))
-                        .uri("lb://student-info-service"))
-                .route("grades_route", r -> r
-                        .path("/api/grades/**", "/api/courses/**")
-                        .filters(f -> f
-                                .rewritePath("/api/grades/(?<segment>.*)", "/grades/${segment}")
-                                .rewritePath("/api/courses/(?<segment>.*)", "/courses/${segment}"))
-                        .uri("lb://grades"))
-                .route("catalog_route", r -> r
-                        .path("/api/catalog/**")
-                        .filters(f -> f.rewritePath("/api/catalog/(?<segment>.*)", "/catalog/${segment}"))
-                        .uri("lb://student-catalog-service"))
+                .route("student",r->r.path("/api/student/**").uri("lb://student-info-service"))
+                .route("course",r->r.path("/api/course/**").uri("lb://grade-data-service"))
+                .route("grade",r->r.path("/api/grade/**").uri("lb://grade-data-service"))
+                .route("catalog",r->r.path("/api/catalog/**").uri("lb://student-catalog-service"))
                 .build();
     }
 }
